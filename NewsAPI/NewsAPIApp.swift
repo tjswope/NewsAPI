9d86b167f1168396de10dc0a3e35f7e2ce8b0aa9
Tom Swope <tjswope@gmail.com>
2024-12-09 14:16:35 -0500

    Initial Commit

---
+++ NewsAPI/NewsAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAPIApp.swift
+//  NewsAPI
+//
+//  Created by Swope, Thomas on 12/9/24.
+//
+
+import SwiftUI
+
+@main
+struct NewsAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
